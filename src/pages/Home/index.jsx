9b3ec1fd5a97b3a2f06f8aa977b233b2d8f23362
Kumar Ambuj <kumarambuj1997@gmail.com>
2024-04-23 18:28:47 +0530

    removed authentication

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -9,14 +9,14 @@ function Home() {
   const [token, setToken] = useState("");
   const [profile, setProfile] = useState(null);
   const [data, setData] = useState([]);
-  const url = `https://graph.facebook.com/v19.0/me/posts?access_token=${token}`;
+  const url = `https://graph.facebook.com/v19.0/3799971930326902/posts?access_token=EAAFrAe5yoZAEBOZC2ZAq8mWS2BZA9hMvTDjByUqqFoYY20xWgUc7Hu2y5aZBHiqc8DsWu3Tt3OVSvdbdkRRxHwCn6SRlOavuyNSl2igP7p6Vw7ny2Xt4pxKpyakNvWAJeLZBK2jpqlhtA9QGRkZBpGwWYTJDFGrOyxc2ivvm0TJhpXj81obF2ss0i31CsKnMBZAXAzKzdBEUYZAJP6nc2c9pFUxkGrF11JRZBgGfpZC4BJ5BpZCxPtgpIGFl`;
   const url2 =
     "https://graph.facebook.com/v19.0/3799971930326902_2313005489023561?fields=id,message,reactions.summary(total_count).as(reactions_total),reactions.type(LIKE).summary(total_count).as(like_reactions),reactions.type(LOVE).summary(total_count).as(love_reactions),reactions.type(WOW).summary(total_count).as(wow_reactions),reactions.type(HAHA).summary(total_count).as(haha_reactions),reactions.type(SAD).summary(total_count).as(sad_reactions),reactions.type(ANGRY).summary(total_count).as(angry_reactions),comments.summary(total_count),shares&access_token=EAAFrAe5yoZAEBOZBPJ2LOMiJfu3mDc0BmpFt95rHc5TFp1Ntv9LMobmN4czfE310iZBvlHss9qEc5fucH5k22jeE2IAhB5ao1pKshBSikCXZArPudZCp78RF25tsjOWRQC1y35uuUPOZA7sK4XrKq6S9AQL5VNzcJUE1EUvdCkSTVZBX3Q6UWjxoM8ZC";
   async function getData() {
     try {
       const response = await fetch(url);
       const responseJson = await response.json();
-      //console.log(responseJson.data);
+      console.log(responseJson.data);
       setData(responseJson.data);
       navigate("/post", { state: { data: responseJson.data } });
     } catch (error) {
@@ -28,7 +28,15 @@ function Home() {
   //   }, []);
   return (
     <>
-      {!profile ? (
+      <div>
+        <div className="userName">
+          Welcome Ambuj !!!{" "}
+          <button className="myPost" onClick={getData}>
+            My Posts
+          </button>
+        </div>
+      </div>
+      {/* {!profile ? (
         <div>
           <LoginSocialFacebook
             style={{ zIndex: "2" }}
@@ -53,7 +61,7 @@ function Home() {
             </button>
           </div>
         </div>
-      )}
+      )} */}
       <div>
         <div className="homeContainer">
           <div className="homeRow">
--- src/pages/PostDeatail/index.jsx
+++ src/pages/PostDeatail/index.jsx
@@ -10,7 +10,7 @@ function PostDetail() {
   async function getData() {
     try {
       const response = await fetch(
-        `https://graph.facebook.com/v19.0/${postId}?fields=id,message,reactions.summary(total_count).as(reactions_total),reactions.type(LIKE).summary(total_count).as(like_reactions),reactions.type(LOVE).summary(total_count).as(love_reactions),reactions.type(WOW).summary(total_count).as(wow_reactions),reactions.type(HAHA).summary(total_count).as(haha_reactions),reactions.type(SAD).summary(total_count).as(sad_reactions),reactions.type(ANGRY).summary(total_count).as(angry_reactions),comments.summary(total_count),shares&access_token=EAAODFOMXwtABOZBqjZCGbUleap7ZAYxrLBi6Gnxq99op6ZA1OU1OIkBbH8AKxyxXouivLsT3foM50jx76n6rkZAcKUOuZAFDFOnWFq8bKkhYAXgJgrWQIV8uJhWuZAMBZB0KcENZCOAUoQrKnWsuvyNYu8Czu4fpbrf7iZBPpGpCrRCfJhgRRCnI5IbPjO`
+        `https://graph.facebook.com/v19.0/${postId}?fields=id,message,reactions.summary(total_count).as(reactions_total),reactions.type(LIKE).summary(total_count).as(like_reactions),reactions.type(LOVE).summary(total_count).as(love_reactions),reactions.type(WOW).summary(total_count).as(wow_reactions),reactions.type(HAHA).summary(total_count).as(haha_reactions),reactions.type(SAD).summary(total_count).as(sad_reactions),reactions.type(ANGRY).summary(total_count).as(angry_reactions),comments.summary(total_count),shares&access_token=EAAFrAe5yoZAEBOxkzLku99IaHMhXUaQTG83JXasF2VZCFTVJMnfyRb7yqka0a4IIC9wUX1Rol6QSZCKmvl5kY47YUOnkneDRHNEChF2met7utH6BQsjtwl0TQvXBlLVyueONXg88ZBqC608JMLZBTNie8zGOZBfVUrmpoNMgr0dU8TWyLHVFK2e30g`
       );
       const responseJson = await response.json();
       setData(responseJson);
